ec4f86fac8a27e34ba5565b85d2a5f79cdbadcb1
Hackasnack <aheadofyou2@gmail.com>
2026-08-02 13:29:22 -0700

    added original title and color

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ let button = document.querySelector('button')
 button.addEventListener('click', convert)
 
 function convert() {
-    let title = document.querySelector('input').value.toLowerCase()
+    let originalTitle = document.querySelector('input').value
+    let title = originalTitle.toLowerCase()
     
     let arr = title.split(' ')
     let newTitle = arr.join('-')
     newTitle = newTitle + '.js'
-    document.querySelector('h3').innerHTML = newTitle
+    document.querySelector('h3').innerHTML = originalTitle
+    document.querySelector('h4').innerHTML = newTitle 
    
 }
